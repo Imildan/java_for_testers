10b26bc2bf0adb173816bde1b43c700ec80f1a5b
imildan <imildan90@gmail.com>
2024-02-12 20:24:29 +0500

    добавлен класс Triangle

---
+++ sandbox/src/main/java/ru/imildan/geometry/figures/Triangle.java
@@ -0,0 +1,54 @@
+package ru.imildan.geometry.figures;
+
+public class Triangle {
+    private double a;
+    private double b;
+    private double c;
+
+    public Triangle(double a, double b, double c) {
+        super();
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public Triangle() {
+        super();
+    }
+
+    public double getA() {
+        return a;
+    }
+
+    public void setA(double a) {
+        this.a = a;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public void setB(double b) {
+        this.b = b;
+    }
+
+    public double getC() {
+        return c;
+    }
+
+    public void setC(double c) {
+        this.c = c;
+    }
+
+    public double squareOfTriangle() {
+        double p = 0.5 * (a + b + c);
+        double s = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+        return s;
+    }
+
+    @Override
+    public String toString() {
+        return "Triangle [a=" + a + ", b=" + b + ", c=" + c + "]";
+    }
+
+}
