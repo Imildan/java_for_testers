b28e728310b5bf59f25e500b8635e24660f23228
imildan <imildan90@gmail.com>
2024-02-12 17:31:34 +0500

    Выполнена структуризация кода

---
+++ sandbox/src/main/java/ru/imildan/geometry/Geometry.java
@@ -0,0 +1,17 @@
+package ru.imildan.geometry;
+
+import ru.imildan.geometry.figures.Rectangle;
+import ru.imildan.geometry.figures.Square;
+
+public class Geometry {
+    public static void main(String[] args) {
+        Square.printSquareArea(7.0);
+        Square.printSquareArea(5.0);
+        Square.printSquareArea(3.0);
+
+        Rectangle.printRectangleArea(3.0, 5.0);
+        Rectangle.printRectangleArea(7.0, 9.0);
+        
+    }
+
+}
