0b5882107a13b3a5c72a1159bd78c546fea5847e
imildan <imildan90@gmail.com>
2024-02-13 17:22:23 +0500

    Добавлен треугольник + вычисление квадратов треугольника

--- sandbox/src/main/java/ru/imildan/geometry/Geometry.java
+++ sandbox/src/main/java/ru/imildan/geometry/Geometry.java
@@ -2,6 +2,8 @@ package ru.imildan.geometry;
 
 import ru.imildan.geometry.figures.Rectangle;
 import ru.imildan.geometry.figures.Square;
+import ru.imildan.geometry.figures.Triangle;
+
 
 public class Geometry {
     public static void main(String[] args) {
@@ -11,7 +13,16 @@ public class Geometry {
 
         Rectangle.printRectangleArea(3.0, 5.0);
         Rectangle.printRectangleArea(7.0, 9.0);
-        
+
+        Triangle triangleOne = new Triangle(2.0, 4.0, 5.0);
+        Triangle triangleTwo = new Triangle(23.3, 23.5, 23.2);
+
+        System.out.println(triangleOne);
+        System.out.println(triangleTwo);
+        System.out.println(triangleOne.squareOfTriangle() + " Квадрат первого треугольника ");
+        System.out.println(triangleTwo.squareOfTriangle() + " Квадрат второго треугольника ");
+
     }
 
 }
+
--- sandbox/src/main/java/ru/imildan/geometry/figures/Triangle.java
+++ sandbox/src/main/java/ru/imildan/geometry/figures/Triangle.java
@@ -1,9 +1,9 @@
 package ru.imildan.geometry.figures;
 
 public class Triangle {
-    private double a;
-    private double b;
-    private double c;
+    public double a;
+    public double b;
+    public double c;
 
     public Triangle(double a, double b, double c) {
         super();
@@ -45,10 +45,12 @@ public class Triangle {
         double s = Math.sqrt(p * (p - a) * (p - b) * (p - c));
         return s;
     }
-
+    public static void printTriangleArea(Triangle s) {
+        String text = String.format("Площадь треугольника со сторонами %f = %f");
+        System.out.println(text);
+    }
     @Override
     public String toString() {
         return "Triangle [a=" + a + ", b=" + b + ", c=" + c + "]";
     }
-
 }
